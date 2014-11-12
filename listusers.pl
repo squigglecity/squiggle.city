fd06c18d9303ccfc3e3c3e58cdd6b257fb96e7db
root <root@squiggle.city>
2014-11-12 18:49:41 +0000

    rounding up recent changes

--- listusers.pl
+++ listusers.pl
@@ -4,12 +4,21 @@ use strict;
 use warnings;
 use 5.10.0;
 
+# we'll use this to filter out people who haven't logged in.
+# pretty silly!
+my %whitelist;
+my (@lastlog) = split /\n/, `lastlog | grep -v Never | awk '{ print \$1; }'`;
+foreach my $user (@lastlog) {
+  $whitelist{$user} = 1;
+}
+
 opendir(my $dh, '/home/')
   or die "could not open /home/: $!";
 
 my %dirs;
 while (my $dir = readdir $dh) {
   next if $dir =~ /^[.]/;
+  next unless $whitelist{$dir};
   $dirs{$dir} = (stat "/home/$dir/public_html/index.html")[9]; # mtime
 }
 close $dh;
