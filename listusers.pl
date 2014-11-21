e406e12e007929faeeea64fe024aa51458b5d70b
root <root@squiggle.city>
2014-11-21 01:37:47 +0000

    rounding up recent changes

--- listusers.pl
+++ listusers.pl
@@ -16,10 +16,17 @@ opendir(my $dh, '/home/')
   or die "could not open /home/: $!";
 
 my %dirs;
+my %titles;
 while (my $dir = readdir $dh) {
   next if $dir =~ /^[.]/;
   next unless $whitelist{$dir};
-  $dirs{$dir} = (stat "/home/$dir/public_html/index.html")[9]; # mtime
+ 
+  my $index_html_path = "/home/$dir/public_html/index.html";
+
+  if (-e $index_html_path) {
+    $dirs{$dir} = (stat $index_html_path)[9]; # mtime
+    $titles{$dir} = get_title_from_file($index_html_path);
+  }
 }
 close $dh;
 
@@ -27,11 +34,42 @@ sub sort_by_time {
    $dirs{$b} <=> $dirs{$a};
 }
 
-my $list = "<ul>\n";
+my $list = "<table>\n";
 foreach my $key (sort sort_by_time (keys(%dirs))) {
-   $list .= '  <li><a href="/~' . $key . '/">~' . $key . '</a> (' . $dirs{$key} . ')</li>' . "\n";
+   $list .= '  <tr>'
+          . '<td><a href="/~' . $key . '/">~' . $key . '</a></td>'
+          . '<td>' . $titles{$key} . '</td>'
+          . '<td class=tiny>' . $dirs{$key} . '</td>'
+          . "</tr>\n";
 }
 
-$list .= "</ul>";
+$list .= "</table>";
 
 say $list;
+
+sub get_title_from_file {
+  my ($filespec) = @_;
+  my $html = slurp($filespec);
+
+  if ($html =~ m{<title>(.*?)</title>}is) {
+    return $1;
+  }
+  return '';
+}
+
+sub slurp {
+  my ($file) = @_;
+  my $everything;
+
+  open my $fh, '<', $file
+    or die "Couldn't open $file: $!\n";
+
+  # line separator:
+  local $/ = undef;
+  $everything = <$fh>;
+
+  close $fh
+    or die "Couldn't close $file: $!";
+
+  return $everything;
+}
